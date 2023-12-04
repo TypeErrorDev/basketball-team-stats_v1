c8ea4581eb85d75a80a450498226887fd13561c1
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2023-12-03 18:45:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 from constants import *
 
 
-print(PLAYERS)
+def clean_data(new_data):
+    cleaned_data = []
+    for player in new_data:
+        int_height = player['height'].split()
+        print(int_height)
+
+
+clean_data()
+# def game_menu():
+#     pass
+
+
+# if __name__ == "__main__":
+#     game_menu()
