7d8a48ecb72b663f67a4bdb16cc3da18f16ac830
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2023-12-03 20:03:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from constants import *
 
+panthers = []
+warriors = []
+bandits = []
+experienced_players = []
+inexperienced_players = []
 
-def clean_data(new_data):
-    cleaned_data = []
-    for player in new_data:
-        int_height = player['height'].split()
-        print(int_height)
+# @PARAMS: PLAYERS
 
 
-clean_data()
+def cleaned_data(new_data):
+    # Height
+    for data in new_data:
+        new_height = data['height'].split()
+        if new_height and new_height[0].isnumeric():
+            data['height'] = int(new_height[0])
+            print(data['height'])
+    # Experience
+        if data["experience"] == "YES":
+            data["experience"] = True
+        else:
+            data["experience"] = False
+        print(data["experience"])
+    # Guardians
+        data["guardians"] = data["guardians"].split(" and ")
+        print(data["guardians"])
+
+
+def sort_experience(data):
+    pass
+
+
+cleaned_data(PLAYERS)
 # def game_menu():
 #     pass
 
